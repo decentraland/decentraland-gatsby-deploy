e0cd17d687bcd996679b9976e231098c7f2a0dbd
Fede <i@2fd.me>
2021-01-30 22:32:20 -0300

    docs: add documentation

--- src/recepies/types.ts
+++ src/recepies/types.ts
@@ -2,7 +2,7 @@ import type * as awsx from "@pulumi/awsx";
 
 export type GatsbyOptions = {
   /**
-   * service name
+   * Service name
    *
    * > This value will be injected as SERVICE_NAME env.
    * > SERVICE_VERSION, SERVICE_DOMAIN, SERVICE_TLD, SERVICE_URL will be also injected
@@ -10,13 +10,43 @@ export type GatsbyOptions = {
   name: string;
 
   /**
-   * path of the content will by upload to the content bucket
+   * Path of the content will by upload to the content bucket
    */
-  contentDirectory: string;
+  contentSource?: string;
 
-  useContentMetadata?: Record<string, Partial<{
-
-  }>>
+  /**
+   * Define a map of redirections, allows you to define file refirections and prefix redirections
+   * (only if contentSource is defined)
+   *
+   * @example redirect a specific path into another path or host
+   *
+   * >  !IMOPRTANT: in order to allow this source path must exists inside the bucket,
+   * >    if the source path is s directory it must containt an `index.html` file
+   *
+   * ```typescript
+   *    {
+   *      '/': '/en/',
+   *      '/me': '/en/me',
+   *      '/submit': '/en/submit',
+   *      '/secment.js': '/s.js',
+   *      '/builder/': 'https://builder.decentraland.org/'
+   *      '/builder/segment.js': 'https://builder.decentraland.org/segment.js',
+   *    }
+   * ```
+   *
+   * @example redirect all path that match with a prefix to another path or host, if target
+   *  ends with `/$1` it will include the path section that match with the wildcard `/*`
+   *
+   * ```typescript
+   *    {
+   *      '/docs/*': '/documentation/',
+   *      '/docs/*': '/documentation/$1',
+   *      '/builder/*': 'https://builder.decentraland.org/',
+   *      '/builder/*': ''https://builder.decentraland.org/$1'
+   *    }
+   * ```
+   */
+  contentRoutingRules?: Record<string, string>
 
   /**
    * define which tld (top level domain will be used)
@@ -42,15 +72,15 @@ export type GatsbyOptions = {
   serviceImage?: string | null | undefined | false
 
   /**
-   * (only if serviceImage it's defined)
-   * The number of instantiations of the service to place and keep running (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
+   * The number of instantiations of the service to place and keep running (only if serviceImage is defined)
    *
    * @default 1
    */
   serviceDesiredCount?: number
 
   /**
-   * (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
    * Memory reserved to the fargare service (in MB)
    *
    * @default 256
@@ -58,8 +88,8 @@ export type GatsbyOptions = {
   serviceMemory?: number
 
   /**
-   * (only if serviceImage it's defined)
-   * Port exposed from fargate service (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
+   * Port exposed from fargate service (only if serviceImage is defined)
    *
    * > This value will be injected as PORT env
    *
@@ -68,7 +98,7 @@ export type GatsbyOptions = {
   servicePort?: number
 
   /**
-   * (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
    * Paths mapped from cloudfront into the fargate service
    * if `false` service will be private
    *
@@ -77,7 +107,7 @@ export type GatsbyOptions = {
   servicePaths?: string[] | false
 
   /**
-   * (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
    * Custom health check path, this endpoint should return a http 200 code
    *
    * @default "/api/status"
@@ -85,13 +115,13 @@ export type GatsbyOptions = {
   serviceHealthCheck?: string
 
   /**
-   * (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
    * Adsitional environment variables exposed into the service
    */
   serviceEnvironment?: awsx.ecs.KeyValuePair[]
 
   /**
-   * (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
    * Create a bucket and inject a AWS_ACCESS_{KEY/SECRET} env with read/create access to that bucket
    *  - if `true` the bucket wont be accesible from the web
    *  - if `string[]` paths will be mapped on cloudfront
@@ -106,7 +136,7 @@ export type GatsbyOptions = {
   useBucket?: boolean | string[]
 
   /**
-   * (only if serviceImage it's defined)
+   * (only if serviceImage is defined)
    * Inject AWS_ACCESS_{KEY/SECRET} env with access to send emails
    *  - if `true` will grant access to send emails using the current domain
    *  - if `string[]` will gran access to end emails using any domain in that list
