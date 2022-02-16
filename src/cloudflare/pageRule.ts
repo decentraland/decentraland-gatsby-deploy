a3fa30501a089fea8fa745bdb9a688909e557b08
frami <i@2fd.me>
2022-02-16 17:35:32 -0300

    feat: add createForwardPageRule

--- src/cloudflare/pageRule.ts
+++ src/cloudflare/pageRule.ts
@@ -2,13 +2,17 @@ import { createHash } from 'crypto'
 import * as cloudflare from '@pulumi/cloudflare'
 import { getZoneId } from 'dcl-ops-lib/cloudflare'
 
+function pageRuleName(service: string, target: string, ruleName: string) {
+  const hash = createHash('sha256').update(target).digest('hex')
+  return [ service, hash, ruleName ].join('-')
+}
+
 /**
  * Create a page rule on cloudflare that cache every thing for a year
  * and send the proper headers to user browser to cache it too
  */
 export function createImmutableCachePageRule(service: string, target: string) {
-  const hash = createHash('sha256').update(target).digest('hex')
-  return new cloudflare.PageRule(`${service}-${hash.slice(0, 4)}-cache`, {
+  return new cloudflare.PageRule(pageRuleName(service, target, 'cache'), {
     target,
     zoneId: getZoneId(),
     actions: {
@@ -17,6 +21,26 @@ export function createImmutableCachePageRule(service: string, target: string) {
       cacheTtlByStatuses: [{ codes: '200', ttl: 31536000 /* a year */ }],
       edgeCacheTtl: 31536000 /* a year */,
       browserCacheTtl: "31536000" /* a year */,
+    }
+  })
+}
+
+/**
+ * Create a page rule on cloudflare that redirects any request matching with `target` to `destination`,
+ * additionally the status code can be defined explicitly (302 as default)
+ *
+ * `source` and `destination` can use cloudfrony match patter
+ * @see https://developers.cloudflare.com/pages/platform/redirects
+ */
+export function createForwardPageRule(service: string, source: string, destination: string, statusCode: 301 | 302 | 303 | 307 | 308 = 302) {
+  return new cloudflare.PageRule(pageRuleName(service, source, 'forward'), {
+    target: source,
+    zoneId: getZoneId(),
+    actions: {
+      forwardingUrl: {
+        statusCode,
+        url: destination
+      }
     }
   })
 }
