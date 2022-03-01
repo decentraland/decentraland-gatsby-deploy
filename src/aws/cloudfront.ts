b0edbe6f0ddfb7f180ba6e758dfaf4cf76897616
frami <i@2fd.me>
2022-03-01 12:47:46 -0300

    fix: customize PermissonPolicy

--- src/aws/cloudfront.ts
+++ src/aws/cloudfront.ts
@@ -37,12 +37,142 @@ export function uniqueOrigins(origins: aws.types.input.cloudfront.DistributionOr
             SECURITY HEADER CONFIGURATION
 *******************************************************/
 
-export type SecurityHeadersOptions = {
-  accessControlAllowCredentials?: boolean
-  accessControlAllowOrigins?: string[],
-  accessControlAllowHeaders?: string[],
-  accessControlAllowMethods?: ("OPTIONS" | "HEAD" | "GET" | "POST" | "PUT" | "PATCH" | "DELETE")[],
-  contentSecurityPolicyScript?: string[],
+export type SecurityHeadersOptions = Partial<{
+  accessControlAllowCredentials: boolean
+  accessControlAllowOrigins: string[],
+  accessControlAllowHeaders: string[],
+  accessControlAllowMethods: ("OPTIONS" | "HEAD" | "GET" | "POST" | "PUT" | "PATCH" | "DELETE")[],
+  contentSecurityPolicyScript: string[],
+  permissonPolicy: Partial<PermissionPolicyOptions>
+}>
+
+export type PermissionPolicyOptions = {
+  // Standardized Features
+  /** The Accelerometer interface of the Sensor APIs provides on each reading the acceleration applied to the device along all three axes. */
+  "accelerometer": boolean | '*' | string[]
+
+  /** The AmbientLightSensor interface of the Sensor APIs returns the current light level or illuminance of the ambient light around the hosting device. */
+  "ambient-light-sensor": boolean | '*' | string[]
+
+  /** Controls the ability to have Media (Audio or Video) elements begin playback without user interaction in the current document. When this policy is disabled and there were no user gestures, the Promise returned by HTMLMediaElement.play() will reject with a DOMException. The autoplay attribute on <audio> and <video> elements will be ignored. */
+  "autoplay": boolean | '*' | string[]
+
+  /** The Battery Status API can be used to defer or scale back work when the device is not charging in or is low on battery. */
+  "battery": boolean | '*' | string[]
+
+  /** Manages access to Camera interfaces (physical and virtual). */
+  "camera": boolean | '*' | string[]
+
+  /** Cross-origin isolation enables a web page to use powerful features such as SharedArrayBuffer, performance.measureUserAgentSpecificMemory(), high resolution timer with better precision, or the JS Self-Profiling API. This also impacts the "document-domain" permission when set (see below). */
+  "cross-origin-isolated": boolean | '*' | string[]
+
+  /** A document's permissions policy determines whether any content in that document is allowed to use getDisplayMedia. */
+  "display-capture": boolean | '*' | string[]
+
+  /** Provides access to the deprecated "document.domain[=domain]" setter. When the "document-domain" feature is disabled, the setter will throw a "SecurityError" exception. In cases where crossOriginIsolated or originAgentCluster return true, the setter will do nothing. It is recommended to avoid using the document.domain setter, instead, use postMessage() or MessageChannel objects to communicate across origins in a safe manner. */
+  "document-domain": boolean | '*' | string[]
+
+  /** Encrypted Media Extensions provides an API that enables web applications to interact with content protection systems, to allow playback of encrypted audio and video. Provides access to the requestMediaKeySystemAccess() method, a part of the MediaKeys object. */
+  "encrypted-media": boolean | '*' | string[]
+
+  /** Controls if tasks should execute for nested browsing contexts (eg. iframes) when it has/is not being rendered. */
+  "execution-while-not-rendered": boolean | '*' | string[]
+
+  /** Controls if tasks should execute for nested browsing contexts (eg. iframes) when not within the current viewport. */
+  "execution-while-out-of-viewport": boolean | '*' | string[]
+
+  /** Determines whether any content in a document is allowed to go fullscreen. If disabled in any document, no content in the document will be allowed to use fullscreen. */
+  "fullscreen": boolean | '*' | string[]
+
+  /** The Geolocation API provides access to geographical location information associated with the host device. */
+  "geolocation": boolean | '*' | string[]
+
+  /** Gyroscope sensor interface to monitor the rate of rotation around the three local primary axes of the device. */
+  "gyroscope": boolean | '*' | string[]
+
+  /** Controls whether the getLayoutMap() method is exposed on the "Keyboard" interface. */
+  "keyboard-map": boolean | '*' | string[]
+
+  /** Magnetometer sensor interface to measure a magnetic field in the X, Y and Z axis. */
+  "magnetometer": boolean | '*' | string[]
+
+  /** Manages access to Microphone interfaces (physical and virtual). */
+  "microphone": boolean | '*' | string[]
+
+  /** Musical Instrument Digital Interface (MIDI) protocol enables electronic musical instruments, controllers and computers to communicate and synchronize with each other. */
+  "midi": boolean | '*' | string[]
+
+  /** Enables the page author to take control over the behavior of spatial navigation, or to cancel it outright. Spatial navigation is the ability to move around the page directionally which can be useful for a web page built using a grid-like layout, or other predominantly non linear layouts. More often this is used in browsers on devices with limited input control, such as a TV. */
+  "navigation-override": boolean | '*' | string[]
+
+  /** Allow merchants (i.e. web sites selling physical or digital goods) to utilise one or more payment methods with minimal integration. */
+  "payment": boolean | '*' | string[]
+
+  /** Allow websites to create a floating video window always on top of other windows so that users may continue consuming media while they interact with other content sites, or applications on their device. This item controls whether the request Picture-in-Picture algorithm may return a SecurityError and whether pictureInPictureEnabled is true or false. */
+  "picture-in-picture": boolean | '*' | string[]
+
+  /** Determines whether any content in the allowed documents is allowed to successfully invoke the Web Authentication API. If disabled in any document, no content in the document will be allowed to use the foregoing methods, attempting to do so will return an error. */
+  "publickey-credentials-get": boolean | '*' | string[]
+
+  /** A screen wake lock prevents the screen from turning off. Only visible documents can acquire the screen wake lock. */
+  "screen-wake-lock": boolean | '*' | string[]
+
+  /** The sync-xhr policy controls whether synchronous requests can be made through the XMLHttpRequest API. If disallowed in a document, then calls to send() on XMLHttpRequest objects with the synchronous flag set will fail, causing a NetworkError DOMException to be thrown. */
+  "sync-xhr": boolean | '*' | string[]
+
+  /** The WebUSB API provides a way to safely expose USB device services to the web. Controls whether the usb attribute is exposed on the Navigator object. */
+  "usb": boolean | '*' | string[]
+
+  /** Exposes the navigator.share() API where supported, which shares the current URL via user agent provided share to locations. */
+  "web-share": boolean | '*' | string[]
+
+  /** The WebXR Device API provides the interfaces necessary to enable developers to build compelling, comfortable, and safe immersive applications on the web across a wide variety of hardware form factors. */
+  "xr-spatial-tracking": boolean | '*' | string[]
+
+  // Proposed Features
+
+  /** Read from the device clipboard via the Clipboard API */
+  "clipboard-read": boolean | '*' | string[]
+
+    /** Write to the device clipboard via the Clipboard API */
+  "clipboard-write": boolean | '*' | string[]
+
+  /** Determines whether any content in that document is allowed to access getGamepads(). If disabled in any document, no content in the document will be allowed to use getGamepads(), nor will the "gamepadconnected" and "gamepaddisconnected" events fire. */
+  "gamepad": boolean | '*' | string[]
+
+  /** Determines whether any content in a document is allowed to use the selectAudioOutput function to prompt the user to select an audio output device, or allowed to use setSinkId to change the device through which audio output should be rendered, to a non-system-default user-permitted device. */
+  "speaker-selection": boolean | '*' | string[]
+
+  // Experimental Features
+  /** Click Through Attribution Reporting. To enable this, use the Chrome command line flag --enable-blink-features=ConversionMeasurement */
+  "conversion-measurement": boolean | '*' | string[]
+
+    /** Helps control the use of automated focus in a main frame or <iframe>. The proposed feature provides a means for developers to block the use of automatic focus in nested contents. */
+  "focus-without-user-activation": boolean | '*' | string[]
+
+  /** Allow a web page to communicate with HID devices (Human Interface Device) */
+  "hid": boolean | '*' | string[]
+
+  /** Allow usage of the IdleDetector interface to better detect if a user is at their device, instead of trying to identify if a user has just become inactive, such as left window open, screen saver activated, screen turned off, changed tabs or changed applications. */
+  "idle-detection": boolean | '*' | string[]
+
+  /** [Obsolete] A site can forbid topic calculation on a page by disabling the interest-cohort. The Topics API replaced Federated Learning of Cohorts (FLoC) which is what this was for, however the Topics API honors the disable setting. The intent of the Topics API is to provide callers (including third-party ad-tech or advertising providers on the page that run scripts) with coarse-grained advertising topics that the page visitor might currently be interested in. */
+  "interest-cohort": boolean | '*' | string[]
+
+  /** Provide direct communication between a web site and the device that it is controlling via a Serial port. To enable this, use the Chrome command line flag --enable-blink-features=Serial */
+  "serial": boolean | '*' | string[]
+
+  /** Unknown - No information currently available. To enable this, use the Chrome command line flag --enable-blink-features=ExperimentalProductivityFeatures. */
+  "sync-script": boolean | '*' | string[]
+
+  /** This API proposes a new per-origin storage area for “Privacy Pass” style cryptographic tokens, which are accessible in third party contexts. These tokens are non-personalized and cannot be used to track users, but are cryptographically signed so they cannot be forged. */
+  "trust-token-redemption": boolean | '*' | string[]
+
+  /** Proposal to provide additional informatiion for Multi-Screen Window Placement. */
+  "window-placement": boolean | '*' | string[]
+
+  /** Vertical scroll policy is a feature introduced to assist websites in blocking certain embedded contents from interfering with vertical scrolling. Stopping a user from vertically scrolling the page might be a frustrating experience. */
+  "vertical-scroll": boolean | '*' | string[]
 }
 
 export const ContentSecurityPolicyScript = {
@@ -74,6 +204,25 @@ export const ContentSecurityPolicyScript = {
   ],
 }
 
+function toPermissionPolicy(options: Partial<PermissionPolicyOptions> = {}): string {
+  return Object.keys(options).map(key => {
+    const value = options[key] as boolean | '*' | string[]
+    switch (value) {
+      case false:
+        return key + '=()'
+      case true:
+        return key + '=(self)'
+      case '*':
+        return key + '=*'
+      default:
+        return key + '=(' + value.map(domain => `"${domain}"`).join(' ') + ')'
+    }
+  }).join(', ')
+}
+
+/**
+ * Create a aws.cloudfront.ResponseHeadersPolicy instance
+ */
 export function staticSecurityHeadersPolicy(serviceName: string, options: SecurityHeadersOptions = {}) {
   return new aws.cloudfront.ResponseHeadersPolicy(`${serviceName}-security-headers`, {
     comment: "Security headers for static files",
@@ -175,12 +324,12 @@ export function staticSecurityHeadersPolicy(serviceName: string, options: Securi
 
     customHeadersConfig: {
       items: [
-        {
+        options.permissonPolicy ?? {
           override: false,
           header: 'Permissions-Policy',
-          value: "'self'"
+          value: toPermissionPolicy(options.permissonPolicy)
         }
-      ]
+      ].filter(Boolean) as aws.types.input.cloudfront.ResponseHeadersPolicyCustomHeadersConfigItem[]
     }
   })
 }
