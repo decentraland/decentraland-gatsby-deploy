365e9c3ef81b36469bde97660ab33a276ed4a3e3
frami <i@2fd.me>
2022-03-09 11:51:03 -0300

    feat: add buildContentBucket acl option

--- src/recipes/buildContentBucket.ts
+++ src/recipes/buildContentBucket.ts
@@ -1,15 +1,36 @@
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 import { routingRules } from "../aws/s3";
 import { getServiceName, getServiceSubdomain } from "../utils";
 import { GatsbyOptions } from "./types";
 
-export function buildContentBucket(config: Pick<GatsbyOptions,  'name' | 'usePublicTLD' | 'contentRoutingRules'>) {
+export type ContentBucketOptions = Pick<GatsbyOptions,  'name' | 'usePublicTLD' | 'contentRoutingRules'> &
+  {
+    /**
+     * The [canned ACL](https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl) to apply.
+     * - **`private`**: Owner gets FULL_CONTROL. No one else has access rights (default).
+     * - **`public-read`**: Owner gets FULL_CONTROL. The AllUsers group gets READ access.
+     * - **`public-read-write`**: Owner gets FULL_CONTROL. The AllUsers group gets READ and WRITE access. Granting this on a bucket is generally not recommended.
+     * - **`aws-exec-read`**: Owner gets FULL_CONTROL. Amazon EC2 gets READ access to GET an Amazon Machine Image (AMI) bundle from Amazon S3.
+     * - **`authenticated-read`**: Owner gets FULL_CONTROL. The AuthenticatedUsers group gets READ access.
+     * - **`bucket-owner-read`**: Object owner gets FULL_CONTROL. Bucket owner gets READ access. If you specify this canned ACL when creating a bucket, Amazon S3 ignores it.
+     * - **`bucket-owner-full-control`**: Both the object owner and the bucket owner get FULL_CONTROL over the object. If you specify this canned ACL when creating a bucket, Amazon S3 ignores it.
+     * - **`log-delivery-write`**: The LogDelivery group gets WRITE and READ_ACP permissions on the bucket. For more information about logs.
+     *
+     */
+    acl: pulumi.Input<'private' | 'public-read' | 'public-read-write' | 'aws-exec-read' | 'authenticated-read' | 'log-delivery-write'>
+  }
+
+/**
+ * Create a S3 bucket
+ */
+export function buildContentBucket(config: Pick<GatsbyOptions,  'name' | 'usePublicTLD' | 'contentRoutingRules'> & { acl: 'private' | 'public-read' | 'public-read-write' | 'aws-exec-read' | 'authenticated-read' | 'log-delivery-write' }) {
   const serviceName = getServiceName(config)
   const serviceDomain = getServiceSubdomain(config)
   const contentRoutingRules = routingRules(config.contentRoutingRules, { hostname: serviceDomain, protocol: 'https' })
   // contentBucket is the S3 bucket that the website's contents will be stored in.
   const contentBucket = new aws.s3.Bucket(`${serviceName}-website`, {
-    acl: "private",
+    acl: config.acl ?? "private",
 
     tags: {
       Name: serviceDomain
